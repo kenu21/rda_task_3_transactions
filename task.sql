57d26a217369131452439728553019aea5a7aa8f
Yurii Keniu <kenu21@gmail.com>
2025-09-19 16:57:15 +0200

    transaction: added sql with transaction

--- task.sql
+++ task.sql
@@ -1,11 +1,11 @@
--- Use our database
-USE ShopDB; 
+USE ShopDB;
+INSERT INTO Orders (CustomerID, Date)
+ VALUES (1, '2023-01-01');
 
--- Some data should be created outside the transaction (here)
-
--- Start the transaction 
-START TRANSACTION; 
-
--- And some data should be created inside the transaction 
-
-COMMIT; 
+START TRANSACTION;
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+ VALUES (1, 1, 1);
+UPDATE Products
+ SET WarehouseAmount = WarehouseAmount - 1
+ WHERE ID = 1;
+COMMIT;
