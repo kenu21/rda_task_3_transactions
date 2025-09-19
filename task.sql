e375cc6b2e92368f6525dab1db0db6c144736066
Yurii Keniu <kenu21@gmail.com>
2025-09-19 17:44:41 +0200

    transaction: improved

--- task.sql
+++ task.sql
@@ -1,11 +1,20 @@
+DROP DATABASE IF EXISTS ShopDB;
+SOURCE create-database.sql;
 USE ShopDB;
-INSERT INTO Orders (CustomerID, Date)
- VALUES (1, '2023-01-01');
 
 START TRANSACTION;
+INSERT INTO Orders (CustomerID, `Date`)
+ VALUES (1, '2023-01-01');
+SET @order_id = LAST_INSERT_ID();
 INSERT INTO OrderItems (OrderID, ProductID, Count)
- VALUES (1, 1, 1);
+ VALUES (@order_id, 1, 1);
 UPDATE Products
  SET WarehouseAmount = WarehouseAmount - 1
- WHERE ID = 1;
+ WHERE ID = 1 AND WarehouseAmount >= 1;
+
+IF ROW_COUNT() = 0 THEN
+	ROLLBACK;
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Product not found';
+END IF;
+
 COMMIT;
